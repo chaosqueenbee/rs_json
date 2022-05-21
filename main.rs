eca2b972a0dea7164eede1ccbbc9dca2df94207c
amarokkin <jasminesingh.2011@gmail.com>
2022-05-21 12:21:12 -0700

    Map of key-value pairs of string, T where T is an i32.

--- main.rs
+++ main.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap;
 
 fn main() {
-    let map: HashMap<&str, i32> = HashMap::new();
+    let mut map: HashMap<&str, i32> = HashMap::new();
+    map.insert("Dalton", 1);
+    map.insert("Jasmine", 0);
     println!("{}", rs_json::dump::<i32>(map));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,25 @@ pub fn load<T>(_s: &str) -> HashMap<&str, T> {
     return HashMap::new();
 }
 
-pub fn dump<T>(_dict: HashMap<&str, T>) -> &str {
-    return "Not Implemented";
+pub fn dump<T: std::fmt::Debug>(dict: HashMap<&str, T>) -> String {
+    // (1) Map of key-value pairs of string, T
+    // "{"key1":"value1","key2":"value2"}"
+    let mut result = "{".to_owned();
+    let mut idx: i32 = 0;
+
+    for (key, value) in dict {
+        if idx != 0 {
+            result.push_str(",")
+        }
+        let key_value = format!("\"{}\":{:?}", key, value);
+        result.push_str(&key_value);
+
+        idx += 1;
+    }
+
+    result.push_str("}");
+
+    return result;
 }
 
 #[cfg(test)]
